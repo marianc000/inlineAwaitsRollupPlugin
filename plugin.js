3fab9954a09b5b44379f85ccd3fd625071b6c2a7
mcaikovs <marian.caikovski@gmail.com>
2021-04-28 13:15:14 +0200

    first

---
+++ plugin.js
@@ -0,0 +1,20 @@
+const path = require('path');
+const { readFileSync } = require('fs');
+ 
+function ImportmapPlugin({ imports } = { imports: [] }) {
+    const re = /await +loadHTML\('([^,]+)',/g;
+
+    return {
+        transform(code, id) {
+            function replacer(match, relativeUrl) {
+                const filePath = path.resolve(path.dirname(id), relativeUrl);
+                const html = readFileSync(filePath,{encoding  : 'utf8'});
+                return JSON.stringify(html) + "; //";
+            }
+            
+            return code.replace(re, replacer);
+        }
+    };
+}
+
+module.exports = ImportmapPlugin;
