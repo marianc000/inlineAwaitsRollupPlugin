bb9de26cafe7f6d9a7f9e1efc0c45ad5cc576725
mcaikovs <marian.caikovski@gmail.com>
2021-04-28 23:57:55 +0200

    first

--- plugin.js
+++ plugin.js
@@ -1,6 +1,6 @@
 const path = require('path');
 const { readFileSync } = require('fs');
- 
+
 function ImportmapPlugin() {
     const re = /await +loadHTML\('([^,]+)',/g;
 
@@ -8,10 +8,10 @@ function ImportmapPlugin() {
         transform(code, id) {
             function replacer(match, relativeUrl) {
                 const filePath = path.resolve(path.dirname(id), relativeUrl);
-                const html = readFileSync(filePath,{encoding  : 'utf8'});
+                const html = readFileSync(filePath, { encoding: 'utf8' });
                 return JSON.stringify(html) + "; //";
             }
-            
+
             return code.replace(re, replacer);
         }
     };
